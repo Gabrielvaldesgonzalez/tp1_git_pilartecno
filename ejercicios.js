050c2f624ad927aba86d78445c0a0f17366e9941
Gabriel Valdes Gonzalez <gabrielvaldesg@gmail.com>
2023-06-23 16:22:02 -0300

    Resolucion del segundo ejercicio de Java

--- ejercicios.js
+++ ejercicios.js
@@ -8,3 +8,21 @@
 function extractValues(data) {
     return Object.values(data);
   }
+  /*  
+  Ejercicio 2: 
+  Implementar la función 'arrayToLengthCounters' que toma un array de strings y retorna un objeto donde cada elemento
+  del array se convierte en una clave del objeto y el valor es la longitud de cada string.
+  Ejemplo: 
+  arrayToLengthCounters(['gato', 'perro', 'elefante']) debería retornar {gato: 4, perro: 5, elefante: 8}
+*/
+
+function arrayToLengthCounters(animales) {
+    let animalLengths = {};
+  
+    animales.forEach(function(animal) {
+      animalLengths[animal] = animal.length;
+    });
+  
+    return animalLengths;
+  }
+  
